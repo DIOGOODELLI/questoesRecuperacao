44d46da05c8849411a6c7ef7e4edd8a2d55b44c5
Diogo Odelli <Diogo Odelli@Diogo-DELL>
2015-10-12 11:46:58 -0300

    commit inicial

---
+++ questoesRecuperacao/src/java/contato.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Diogo Odelli
+ */
+@WebServlet(urlPatterns = {"/contato"})
+public class contato extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+           
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Com Servelet</h1>");
+            out.println("<form method='post'>");
+            out.println("Remetente:<br />");
+            out.println("<input type='text' />");
+            out.println("<br /><br />");
+            out.println("Assunto<br />");
+            out.println("<input type='text'/>");
+            out.println("<br /><br />");
+            out.println("Descrição<br />");
+            out.println("<textarea rows='4' cols='50'></textarea>");
+            out.println("<br /><br />");
+            out.println("<input type='submit' value='Enviar' />");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ questoesRecuperacao/web/questao1/web/contato.jsp
@@ -0,0 +1,21 @@
+
+<html>
+    <head>
+    <title>Contato</title>            
+    </head> 
+    <body>
+    <h1>Contato</h1>
+    <form method='post'>
+        Remetente:<br />
+        <input type='text' />
+        <br /><br />
+        Assunto<br />
+        <input type='text'/>
+        <br /><br />
+        Descrição<br />
+        <textarea rows="4" cols="50"></textarea>
+        <br /><br />
+        <input type='submit' value='Enviar' />
+    </form>
+    </body>
+</html>
